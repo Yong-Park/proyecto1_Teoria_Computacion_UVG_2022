5d79791de52bd6801936bb375acf04cfbce2e6ba
yong park <yong.park123456@gmail.com>
2022-09-27 12:48:25 -0600

    ya se logra la minimizacion pero no es 100% seguro entonces realizar mas pruebas

--- minimization.py
+++ minimization.py
@@ -10,6 +10,7 @@ def minimization(afd, inicio, final):
     transaction = []
     equivalents = []
     extra = []
+    afd_minimizado = {}
     
     #guardar las cadenas segun son terminales o no
     for element in afd:
@@ -18,48 +19,96 @@ def minimization(afd, inicio, final):
         else:
             not_end.append(element)
     #estos son los 0-equivalentes
-    P0.append(end)
     P0.append(not_end)
+    P0.append(end)
     
     #guardar los valores de las transaciones utilizadas
     for element in afd:
         for values in afd[element]:
             if values not in transaction:
                 transaction.append(values)
-    print(transaction)
+    # print(transaction)
     
-    #comenzar con la rotacion de equivalentes 
-    count = 1
+    contador = 1
     largo = 0
-    while largo != len(P0):
+    while(largo != len(P0)):
         largo = len(P0)
-        print(str(count) + "-equivalente")
+        # print(str(contador) + "-equivalentes")
+        #comenzar con la rotacion de equivalentes 
         for x in P0:
+            position = 0
             if len(x) > 1:
-                #revisar
+                equivalents = []
+                extra = []
+                equivalents.append(x[position])
                 for y in x:
-                    for z in x:
-                        if y != z:
-                            times = 0
-                            for val in transaction:
-                                if afd[y][val] == afd[z][val]:
-                                    times += 1
-                                else:
-                                    if afd[y][val] in x and afd[z][val] in x:
-                                        times += 1
-                                    
-                            if times == 2:
-                                if y not in equivalents:
-                                    equivalents.append(y)
-                                if z not in equivalents:
-                                    equivalents.append(z)
-                break
-            equivalents.append(equivalents)
+                    if x[position] != y:
+                        counter = 0
+                        for val in transaction:
+                            if afd[x[position]][val] == afd[y][val]:
+                                counter += 1
+                            else:
+                                # print("=============")
+                                # print(afd[x[position]][val])
+                                # print(afd[y][val])
+                                # print (x)
+                                # print("=============")
+                                if str(afd[x[position]][val]) in x and str(afd[y][val]) in x:
+                                    counter +=1
+                        # print("counter: " + str(counter))
+                        if counter == 2:
+                            if y not in equivalents:
+                                equivalents.append(y)
+                        else:
+                            if y not in extra:
+                                extra.append(y)
+                    # print("equivalent: " + str(equivalents))
+                    # print("extra: " + str(extra))
+                
+                if len(equivalents) > 0:
+                    P0.append(equivalents)
+                if len(extra) > 0:
+                    P0.append(extra)
+                P0.remove(x)
+        contador += 1
+        # print(P0)
+        
+    print("\nTabla afd minimizado")
+    print(P0)
+            
+    #construcion de su tabla
+    for x in P0:
+        afd_minimizado[x[0]] = {}
+        for val in transaction:
+            if str(afd[x[0]][val]) in x:
+                afd_minimizado[x[0]][val] = x[0]
+            else:
+                afd_minimizado[x[0]][val] = afd[x[0]][val]
+                
+    tab = pd.DataFrame(afd_minimizado)
+    print(tab.transpose())
+    
+     #mostrar su grafo
+    f = graphviz.Digraph(comment = "afd minimzado")
+    names = []
+    for i in P0:
+        names.append(i[0])
+    for name in names:
+        if final in (name):
+            f.node(str(name),shape="doublecircle")
+        else:
+            f.node(str(name))
+        
+    f.node("", shape="plaintext")
+    for l in afd_minimizado: #los nodos
+        if inicio in l:
+            f.edge("",str(l),label="")
+        for v in afd_minimizado[l]: #sus variables                
+            f.edge(str(l),str(afd_minimizado[l][v]),label = str(v))
+        
+        
+    f.render("afd minimizado", view = True)
+        
                                 
-                                
-                        
-                    
-        print(equivalents)
-        count +=1               
         
    
